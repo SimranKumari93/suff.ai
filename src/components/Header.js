960f31175f61e19d5c833269ea68ed6da911990c
simran <simrankumaribodhgaya93@gmail.com>
2023-12-01 20:05:32 +0530

    deployed on vercel

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,20 @@
 import React from 'react'
 import Card from './Card.js';
 import suffaiimg from '../../src/images/suffaiimg.png'
+import india from '../../src/images/india.png' 
+import Background from '../../src/images/Background.png'
 
 const Header = () => {
-      const Location1 = ['Banglore', 'Delhi ', 'Agra' , 'Kolkata'];
-      const Location2 = ['Banglore',' Delhi' , 'Agra ', 'Chennai '];
-      const Location3 = ['Banglore', 'Delhi' , 'Agra' , 'Mumbai' ];
-      const Location4 = ['Powai',' Goa', 'Bhopal', 'DelhiNCR '];
+      const Location1 = ['Banglore', 'Delhi', 'Agra', 'Kolkata'];
+      const Location2 = ['Banglore','Delhi' , 'Chennai' , 'Goa'];
+      const Location3 = ['Banglore', 'Delhi' , 'Mumbai' , 'Bhopal'];
+      // const Location4 = [ 'Goa', 'Bhopal', 'Delhi'];
 
   return (
     <>
     <div className='head'><img src={suffaiimg} alt=""/></div>
+    <marquee><img alt="" src={Background}></img></marquee>
+    <center><h2 className='shops'>Our Trusted Shops<img alt="" src={india}></img>India </h2></center>
     <div className='wrap'>
       <div className='Sec'>
        <div className='subSec'>
@@ -28,11 +32,11 @@ const Header = () => {
         <Card name={suffai} key = {index}/>
         ) )}
        </div>
-       <div className='subSec'>
+       {/* <div className='subSec'>
         {Location4.map( (suffai, index) => (
         <Card name={suffai} key = {index}/>
         ) )}
-       </div>
+       </div> */}
       </div>
     </div>
     </>
--- src/components/SuffDetail.js
+++ src/components/SuffDetail.js
@@ -1,25 +1,47 @@
-// suffDetail.js
-import React from 'react';
-import { useParams } from 'react-router-dom';
-import ReviewList from './ReviewList';
+import React from 'react'
 
-const SuffDetail = ({ suffs }) => {
-  const { id } = useParams();
-  const suff = suffs.find((r) => r.id === parseInt(id));
-
-  if (!suff) {
-    return <div>suff not found</div>;
-  }
-
+function SuffDetail() {
   return (
-    <div className="suff-detail">
-      <h2>{suff.name}</h2>
-      <img src={suff.image} alt={suff.name} />
-      <p>{suff.cuisine}</p>
-      <p>{suff.address}</p>
-      <ReviewList reviews={suff.reviews} />
-    </div>
-  );
-};
+    <div>SuffDetail</div>
+  )
+}
 
-export default SuffDetail;
+export default SuffDetail
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // suffDetail.js
+// import React from 'react';
+// import { useParams } from 'react-router-dom';
+// import ReviewList from './ReviewList';
+
+// const SuffDetail = ({ suffs }) => {
+//   const { id } = useParams();
+//   const suff = suffs.find((r) => r.id === parseInt(id));
+
+//   if (!suff) {
+//     return <div>suff not found</div>;
+//   }
+
+//   return (
+//     <div className="suff-detail">
+//       <h2>{suff.name}</h2>
+//       <img src={suff.image} alt={suff.name} />
+//       <p>{suff.cuisine}</p>
+//       <p>{suff.address}</p>
+//       <ReviewList reviews={suff.reviews} />
+//     </div>
+//   );
+// };
+
+// export default SuffDetail;
