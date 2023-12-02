d7bd00d79cc4dd3e1405f99fa02e03a1609abdcb
simran <simrankumaribodhgaya93@gmail.com>
2023-12-02 15:02:34 +0530

    font changed

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,6 +3,8 @@ import Card from './Card.js';
 import suffaiimg from '../../src/images/suffaiimg.png'
 import india from '../../src/images/india.png' 
 import Background from '../../src/images/Background.png'
+// import login from '../../src/images/login.png'
+// import signup from '../../src/images/signup.png'
 
 const Header = () => {
       const Location1 = ['Banglore', 'Delhi', 'Agra', 'Kolkata'];
@@ -12,9 +14,17 @@ const Header = () => {
 
   return (
     <>
-    <div className='head'><img src={suffaiimg} alt=""/></div>
-    <marquee><img alt="" src={Background}></img></marquee>
-    <center><h2 className='shops'>Our Trusted Shops<img alt="" src={india}></img>India </h2></center>
+    <div className='head'><img src={suffaiimg} alt=""/>
+    {/* <img src={login} alt=""/>
+    <img src={signup} alt=""/> */}
+    </div>
+   <img alt="" src={Background}></img>
+    <center><h2 className='shops-1'>Our Trusted Service Provider <img alt="" src={india}></img> India </h2></center>
+    <center><h4 className='shops-2'>Because your clothes deserve better.<br/>
+    From us and ours, to you and your clothes!<br/>
+    Batt(l)ing against the wind- the superwoman way.<br/>
+    Which is why we wouldn't want you to waste yours on laundry either.<br/>
+    Needless to say, we're huge fans of saving time and money as much as possible.<br/></h4></center>
     <div className='wrap'>
       <div className='Sec'>
        <div className='subSec'>
