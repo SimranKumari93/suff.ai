defd0753072417ab39cb0846b90e837439fb6b9e
simran <simrankumaribodhgaya93@gmail.com>
2023-12-23 01:44:17 +0530

    css improved

--- src/components/ComingSoon.js
+++ src/components/ComingSoon.js
@@ -6,16 +6,16 @@ import { useNavigate } from "react-router-dom";
 import '../styles/ComingSoon.css'
 
 const ComingSoon = () => {
-  const navigate = useNavigate();
+  const navigate = useNavigate(true);
   // const [ canGoBack, setCanGoBack ] = useState(false);
   // const [timeLeft, setTimeLeft] = React.useState(new Date("2023-12-31T00:00:00"));
   // useEffect(() =>{
   //   setCanGoBack(navigate.canGoBack())
   // }, [navigate])
-  // const handleClick = () => {
-  //   // Use the navigate function to navigate to a different route
-  //   navigate('/Header');
-  // };
+  const handleClick = () => {
+    // Use the navigate function to navigate to a different route
+    navigate('/Header');
+  };
   return (
    <div className="launch">
       <center> <img src={suffai} alt="" />
@@ -24,13 +24,13 @@ const ComingSoon = () => {
                 {/* <Countdown date={timeLeft} /> */}
       <h4 style={{color: 'rgb(51, 60, 119)'}}>
         We're working hard to launch our services in your city. <br/>
-        Sign up for our email list to be the first to know when we're available!
+        
       </h4><br/>
       
       {/* <form>
         <input type="email" placeholder="Email Address" />
         <button  type="submit">Notify Me</button><br/><br/> */}
-        <button  onClick={() => {navigate('/Header')}}> Home</button>
+        <button  onClick={handleClick}> Home</button>
       {/* </form> */}
       </center>
     </div>
